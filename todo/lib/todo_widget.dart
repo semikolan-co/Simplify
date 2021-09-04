bf6b9b5d99243c614fddbe5b075ee871064c6c18
Satya Prakash <73766765+satyaprakashsinghrathour@users.noreply.github.com>
2021-09-04 11:53:56 -0700

    Update todo_widget.dart

--- todo/lib/todo_widget.dart
+++ todo/lib/todo_widget.dart
@@ -18,34 +18,18 @@ class ToDoWidget extends StatelessWidget {
               TextFormField(
                 decoration: InputDecoration(
                   hintText: 'Title',
-                  // border: OutlineInputBorder(
-                  //     borderSide: BorderSide(color: Colors.green)),
-                  // enabledBorder: OutlineInputBorder(
-                  //   borderRadius: BorderRadius.all(Radius.circular(12.0)),
-                  //   borderSide: BorderSide(color: Colors.amber, width: 2),
-                  // ),
+                 
                   enabledBorder: InputBorder.none,
-                  // focusedBorder: OutlineInputBorder(
-                  //   borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                  //   borderSide: BorderSide(color: Colors.black),
-                  // ),
+                 
                 ),
               ),
               TextFormField(
                 decoration: InputDecoration(
                   border: InputBorder.none,
                   hintText: 'Note',
-                  // border: OutlineInputBorder(
-                  //     borderSide: BorderSide(color: Colors.green)),
-                  // enabledBorder: OutlineInputBorder(
-                  //   borderRadius: BorderRadius.all(Radius.circular(12.0)),
-                  //   borderSide: BorderSide(color: Colors.amber, width: 2),
-                  // ),
+                 
                   enabledBorder: InputBorder.none,
-                  // focusedBorder: OutlineInputBorder(
-                  //   borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                  //   borderSide: BorderSide(color: Colors.black),
-                  // ),
+                 
                 ),
                 // minLines: 4,
                 maxLines: 5,
